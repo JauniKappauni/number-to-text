d5a6f2b46606e93b2b6321cdafcd6ad91a7eb5e7
JauniKappauni <social.jauni@gmail.com>
2025-12-25 16:39:19 +0100

    feat: process number independent from each other to be able to display e.g. 777 as SEVEN ZERO ZERO instead of SEVEN ZERO

--- main.c
+++ main.c
@@ -6,18 +6,19 @@ int main(void)
     const char *words[] = {"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"};
     printf("Please enter a number between 0 and 999: ");
     scanf("%d", &myNum);
-    if (myNum >= 100 && myNum <= 999)
+    int h = myNum / 100;
+    int t = (myNum / 10) % 10;
+    int o = myNum % 10;
+    if (myNum >= 100)
     {
-        printf("%s ", words[myNum / 100]);
-        myNum = myNum % 100;
+        printf("%s ", words[h]);
     }
-    if (myNum >= 10 && myNum <= 99)
+    if (myNum >= 10)
     {
-        printf("%s ", words[myNum / 10]);
-        myNum = myNum % 10;
+        printf("%s ", words[t]);
     }
-    if (myNum >= 0 && myNum <= 9)
+    if (myNum >= 0)
     {
-        printf("%s ", words[myNum]);
+        printf("%s ", words[o]);
     }
 }
