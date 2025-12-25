fb5a5470b1f70c97e18c197dfa4ed9ef4014436d
JauniKappauni <social.jauni@gmail.com>
2025-12-25 16:31:07 +0100

    refactor: simplify digit extraction and make words array const

--- main.c
+++ main.c
@@ -3,19 +3,17 @@
 int main(void)
 {
     unsigned int myNum;
-    char *words[] = {"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"};
+    const char *words[] = {"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"};
     printf("Please enter a number between 0 and 999: ");
     scanf("%d", &myNum);
     if (myNum >= 100 && myNum <= 999)
     {
-        unsigned int tempVar = myNum / 100;
-        printf("%s ", words[tempVar]);
+        printf("%s ", words[myNum / 100]);
         myNum = myNum % 100;
     }
     if (myNum >= 10 && myNum <= 99)
     {
-        unsigned int tempVar = myNum / 10;
-        printf("%s ", words[tempVar]);
+        printf("%s ", words[myNum / 10]);
         myNum = myNum % 10;
     }
     if (myNum >= 0 && myNum <= 9)
