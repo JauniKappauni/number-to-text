e201411f01f7532ef0ba78574e2eae644d05d403
JauniKappauni <social.jauni@gmail.com>
2026-01-04 22:22:43 +0100

    fix: correct input validation for unsigned integer range

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(void)
     int h = myNum / 100;
     int t = (myNum / 10) % 10;
     int o = myNum % 10;
-    if(myNum < 0 || myNum > 999){
+    if(myNum > 999){
         printf("Please enter a number between -1 and 1000");
     }
     if (myNum >= 100)
